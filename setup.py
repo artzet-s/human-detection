e9c125964d7e5014d819e8bfd39865981cd8aaec
Simon Artzet <simon.artzet@gmail.com>
2020-12-17 11:47:37 +0100

    fix error setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 import setuptools
 
-
 setuptools.setup(
     name="human_detection",
     version="0.0.1",
     author="Simon Artzet",
     author_email="simon.artzet@gmail.com",
     description="Tools for human video detection",
-    packages=setuptools.find_packages('human_detection'),
+    packages=setuptools.find_packages(),
 )
